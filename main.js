ce92f29ab81f084728993527ced5ea897eaa82b6
Patrick Schnetger <patrick.schnetger@gmail.com>
2020-09-19 09:30:30 +0200

    Adds participate module and uses it when the correct command has been executed

--- main.js
+++ main.js
@@ -1,135 +1,16 @@
-const TUESDAY_TIME_DIFF = 2;
-const THURSDAY_TIME_DIFF = 4;
-const SATURDAY_TIME_DIFF = 6;
-
-function getThisWeeksTuesday() {
-  const thisWeeksTuesday = new Date(Date.now());
-  const today = thisWeeksTuesday.getDate();
-  const dayOfTheWeek = thisWeeksTuesday.getDay();
-  thisWeeksTuesday.setDate(today - dayOfTheWeek + TUESDAY_TIME_DIFF);
-  thisWeeksTuesday.setHours(18, 0, 0, 0);
-
-  if (new Date() < thisWeeksTuesday) {
-    return new Date(thisWeeksTuesday);
-  }
-  const nextWeeksTraining = new Date(thisWeeksTuesday);
-  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
-}
-
-function getThisWeeksThursday() {
-  const thisWeeksTuesday = new Date(Date.now());
-  const today = thisWeeksTuesday.getDate();
-  const dayOfTheWeek = thisWeeksTuesday.getDay();
-  thisWeeksTuesday.setDate(today - dayOfTheWeek + THURSDAY_TIME_DIFF);
-  thisWeeksTuesday.setHours(18, 0, 0, 0);
-
-  if (new Date() < thisWeeksTuesday) {
-    return new Date(thisWeeksTuesday);
-  }
-  const nextWeeksTraining = new Date(thisWeeksTuesday);
-  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
-}
-
-function getThisWeeksSaturday() {
-  const thisWeeksSaturday = new Date(Date.now());
-  const today = thisWeeksSaturday.getDate();
-  const dayOfTheWeek = thisWeeksSaturday.getDay();
-  thisWeeksSaturday.setDate(today - dayOfTheWeek + SATURDAY_TIME_DIFF);
-  thisWeeksSaturday.setHours(18, 0, 0, 0);
-
-  if (new Date() < thisWeeksSaturday) {
-    return new Date(thisWeeksSaturday);
-  }
-  const nextWeeksTraining = new Date(thisWeeksSaturday);
-  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
-}
-
-function getTrainingDateAsGermanDateFormat(trainingDate) {
-  return (
-    trainingDate.getUTCDate() +
-    "." +
-    (trainingDate.getMonth() + 1) +
-    "." +
-    trainingDate.getFullYear()
-  );
-}
-
-function trainingDayForChannelId(channelId) {
-  if (channelId === "C012C7UEX9C") {
-    return "Dienstag";
-  }
-
-  if (channelId === "C012K00AJFL") {
-    return "Donnerstag";
-  }
-
-  if (channelId === "C012C7UQPSS") {
-    return "Samstag";
-  }
-}
-
-function getUpcomingTrainingWeekDayDate(trainingDay) {
-  let trainingDate;
-  if (trainingDay === "Dienstag") {
-    trainingDate = getThisWeeksTuesday();
-  } else if (trainingDay === "Donnerstag") {
-    trainingDate = getThisWeeksThursday();
-  } else if (trainingDay === "Samstag") {
-    trainingDate = getThisWeeksSaturday();
-  }
-
-  return getTrainingDateAsGermanDateFormat(trainingDate);
-}
-
-function newSheet(spreadsheetApp, sheetName) {
-  const sheet = spreadsheetApp.insertSheet(sheetName);
-
-  return sheet.appendRow(["Name", "Eingetragen um"]);
-}
-
-function saveNewTrainingAttendeeToSpreadSheet(
-  fullTrainingWeekDayName,
-  upcomingTrainingWeekDayDate,
-  spreadsheetApp,
-  userName
-) {
-  const spreadsheetName =
-    fullTrainingWeekDayName + " " + upcomingTrainingWeekDayDate;
-  let spreadsheet = spreadsheetApp.getSheetByName(spreadsheetName);
-
-  if (spreadsheet == null) {
-    spreadsheet = newSheet(spreadsheetApp, spreadsheetName);
-  }
-
-  spreadsheet.appendRow([userName, new Date().toISOString()]);
-
-  return spreadsheetName;
-}
+const participate = require("./participate");
 
 function doPost(e) {
   if (typeof e === "undefined") {
     return;
   }
 
-  const spreadsheetApp = SpreadsheetApp.getActiveSpreadsheet();
+  let textOutput;
 
-  const userName = e.parameter.user_name;
-  const channelId = e.parameter.channel_id;
+  if (e.parameter.command === "/dabei") {
+    textOutput = participate(e);
+  }
 
-  let fullTrainingWeekDayName = trainingDayForChannelId(channelId);
-  let upcomingTrainingWeekDayDate = getUpcomingTrainingWeekDayDate(
-    fullTrainingWeekDayName
-  );
-
-  let spokenTrainingDate = saveNewTrainingAttendeeToSpreadSheet(
-    fullTrainingWeekDayName,
-    upcomingTrainingWeekDayDate,
-    spreadsheetApp,
-    userName
-  );
-
-  let textOutput =
-    "Du bist beim Training am " + spokenTrainingDate + " dabei :confetti_ball:";
   return ContentService.createTextOutput(textOutput);
 }
 
---
+++ participate.js
@@ -0,0 +1,131 @@
+const TUESDAY_TIME_DIFF = 2;
+const THURSDAY_TIME_DIFF = 4;
+const SATURDAY_TIME_DIFF = 6;
+
+function getThisWeeksTuesday() {
+  const thisWeeksTuesday = new Date(Date.now());
+  const today = thisWeeksTuesday.getDate();
+  const dayOfTheWeek = thisWeeksTuesday.getDay();
+  thisWeeksTuesday.setDate(today - dayOfTheWeek + TUESDAY_TIME_DIFF);
+  thisWeeksTuesday.setHours(18, 0, 0, 0);
+
+  if (new Date() < thisWeeksTuesday) {
+    return new Date(thisWeeksTuesday);
+  }
+  const nextWeeksTraining = new Date(thisWeeksTuesday);
+  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
+}
+
+function getThisWeeksThursday() {
+  const thisWeeksTuesday = new Date(Date.now());
+  const today = thisWeeksTuesday.getDate();
+  const dayOfTheWeek = thisWeeksTuesday.getDay();
+  thisWeeksTuesday.setDate(today - dayOfTheWeek + THURSDAY_TIME_DIFF);
+  thisWeeksTuesday.setHours(18, 0, 0, 0);
+
+  if (new Date() < thisWeeksTuesday) {
+    return new Date(thisWeeksTuesday);
+  }
+  const nextWeeksTraining = new Date(thisWeeksTuesday);
+  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
+}
+
+function getThisWeeksSaturday() {
+  const thisWeeksSaturday = new Date(Date.now());
+  const today = thisWeeksSaturday.getDate();
+  const dayOfTheWeek = thisWeeksSaturday.getDay();
+  thisWeeksSaturday.setDate(today - dayOfTheWeek + SATURDAY_TIME_DIFF);
+  thisWeeksSaturday.setHours(18, 0, 0, 0);
+
+  if (new Date() < thisWeeksSaturday) {
+    return new Date(thisWeeksSaturday);
+  }
+  const nextWeeksTraining = new Date(thisWeeksSaturday);
+  return new Date(nextWeeksTraining.setDate(nextWeeksTraining.getDate() + 7));
+}
+
+function getTrainingDateAsGermanDateFormat(trainingDate) {
+  return (
+    trainingDate.getUTCDate() +
+    "." +
+    (trainingDate.getMonth() + 1) +
+    "." +
+    trainingDate.getFullYear()
+  );
+}
+
+function trainingDayForChannelId(channelId) {
+  if (channelId == "C012C7UEX9C") {
+    return "Dienstag";
+  }
+
+  if (channelId == "C012K00AJFL") {
+    return "Donnerstag";
+  }
+
+  if (channelId == "C012C7UQPSS") {
+    return "Samstag";
+  }
+}
+
+function getUpcomingTrainingWeekDayDate(trainingDay) {
+  let trainingDate;
+  if (trainingDay === "Dienstag") {
+    trainingDate = getThisWeeksTuesday();
+  } else if (trainingDay === "Donnerstag") {
+    trainingDate = getThisWeeksThursday();
+  } else if (trainingDay === "Samstag") {
+    trainingDate = getThisWeeksSaturday();
+  }
+
+  return getTrainingDateAsGermanDateFormat(trainingDate);
+}
+
+function saveNewTrainingAttendeeToSpreadSheet(
+  fullTrainingWeekDayName,
+  upcomingTrainingWeekDayDate,
+  spreadsheetApp,
+  userName,
+  channelName
+) {
+  const spreadsheetName =
+    fullTrainingWeekDayName + " " + upcomingTrainingWeekDayDate;
+  let spreadsheet = spreadsheetApp.getSheetByName(fullTrainingWeekDayName);
+
+  if (spreadsheet == null) {
+    spreadsheet = spreadsheetApp.insertSheet(spreadsheetName);
+  }
+
+  spreadsheet.appendRow([userName, Date.now()]);
+
+  return spreadsheetName;
+}
+
+function participate(e) {
+  if (typeof e === "undefined") {
+    return;
+  }
+
+  const spreadsheetApp = SpreadsheetApp.getActiveSpreadsheet();
+
+  const userName = e.parameter.user_name;
+  const channelName = e.parameter.channel_name;
+  const channelId = e.parameter.channel_id;
+
+  let fullTrainingWeekDayName = trainingDayForChannelId(channelId);
+  let upcomingTrainingWeekDayDate = getUpcomingTrainingWeekDayDate(
+    fullTrainingWeekDayName
+  );
+
+  let spokenTrainingDate = saveNewTrainingAttendeeToSpreadSheet(
+    fullTrainingWeekDayName,
+    upcomingTrainingWeekDayDate,
+    spreadsheetApp,
+    userName,
+    channelName
+  );
+
+  return  "Du bist beim Training am " + spokenTrainingDate + " dabei :confetti_ball:";
+}
+
+module.exports = participate;
