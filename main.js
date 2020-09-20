6e5d125ce900e69de1dd8ce6da3d53e09be14019
lennartblom <lennart@blom.de>
2020-09-20 12:36:11 +0200

    Optional require and switch command identification
    Google App Scripts does not need to require node js modules and
    enables scripts to import stuff directly, when it's somewhere around
    the project. Therefore I defined a optional require statement which is
    only triggered when require is undefined.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const participate = require("./participate");
+const PARTICIPATE_COMMAND = "/dabei";
 
 function doPost(e) {
   if (typeof e === "undefined") {
@@ -7,11 +7,27 @@ function doPost(e) {
 
   let textOutput;
 
-  if (e.parameter.command === "/dabei") {
-    textOutput = participate(e);
+  let command = e.parameter.command;
+
+  switch (command) {
+    case PARTICIPATE_COMMAND: {
+      textOutput = handleParticipate(e);
+    }
   }
 
   return ContentService.createTextOutput(textOutput);
+}
+
+function handleParticipate(e) {
+  let textOutput;
+  if (typeof require === "undefined") {
+    textOutput = participate(e);
+  }
+
+  const participate = require("./participate");
+  textOutput = participate(e);
+
+  return textOutput
 }
 
 exports._test = {
