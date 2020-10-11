086df88a2f349bc99eb15342b4881252a03e4e69
lennartblom <lennart@blom.de>
2020-10-11 18:00:17 +0200

    Add business logic for /austragen command

--- src/crossDomain.js
+++ src/crossDomain.js
@@ -97,4 +97,3 @@ exports.getSpreadsheetName = getSpreadsheetName;
 exports.TUESDAY_CHANNEL_ID = TUESDAY_CHANNEL_ID;
 exports.THURSDAY_CHANNEL_ID = THURSDAY_CHANNEL_ID;
 exports.SATURDAY_CHANNEL_ID = SATURDAY_CHANNEL_ID;
-
---
+++ src/signOut.js
@@ -0,0 +1,42 @@
+const {getSpreadsheetName} = require("./crossDomain");
+
+function signOut(e) {
+  if (typeof e === "undefined") {
+    return;
+  }
+
+  const channelId = e.parameter.channel_id;
+  const userName = e.parameter.user_name;
+  const spreadsheetApp = SpreadsheetApp.getActiveSpreadsheet();
+
+  let spreadsheetName = getSpreadsheetName(channelId);
+
+  let trainingSpreadSheet = spreadsheetApp.getSheetByName(spreadsheetName);
+
+  const participantsRowIndex = getParticipantsRowIndex(trainingSpreadSheet, userName);
+  if (participantsRowIndex < 0) {
+    return ["Du bist gar nicht beim Training angemeldet", false];
+  }
+  trainingSpreadSheet.deleteRow(participantsRowIndex + 1);
+  return ["Du hast dich erfolgreich vom Training abgemeldet", true];
+}
+
+function getParticipantsRowIndex(trainingSpreadSheet, userName) {
+  const participantsRows = trainingSpreadSheet.getDataRange().getValues();
+
+  let counter = 0;
+  let indexToFind = -1;
+  participantsRows.forEach(function (row) {
+    if (row[0] === userName) {
+      indexToFind = counter;
+    }
+
+    counter++;
+  });
+
+  return indexToFind;
+}
+
+module.exports = {
+  signOut: signOut
+}
