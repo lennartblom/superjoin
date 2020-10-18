2beb28b049fa8dd88a0b8e755151a1073e85ca82
lennartblom <lennart@blom.de>
2020-10-18 15:42:03 +0200

    Add enable functionality for mondays and sundays

--- test/testParticipate.js
+++ test/testParticipate.js
@@ -7,6 +7,43 @@ global.ContentService = {
 };
 
 describe("`/dabei` command", function () {
+
+  it("should return Monday when being in #training-montag channel", function () {
+    global.SpreadsheetApp = {
+      getActiveSpreadsheet: () => ({
+        getSheetByName: () => ({
+          appendRow: () => ({}),
+          getDataRange: () => ({
+            getValues: () => [
+              [""], ["max mustermann", 1337], ["alina musterfrau", 1337]
+            ]
+          }),
+        }),
+      }),
+    };
+
+    const e = {
+      parameter: {
+        user_name: "wolfgang",
+        command: "/dabei",
+        channel_id: "C01BXQKR9KM",
+        channel_name: "training-samstag",
+      },
+    };
+
+    let thisWeeksSaturday = getThisWeeksDay(false, 1);
+    let nextWeeksSaturday = getThisWeeksDay(true, 1);
+    let returnValue = participate(e);
+
+    if (thisWeeksSaturday < Date.now()) {
+      console.log("Expect next weeks Saturday");
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksSaturday, "Montag"));
+    } else {
+      console.log("Expect this weeks Saturday");
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksSaturday, "Montag"));
+    }
+    assert.strictEqual(returnValue[1], true);
+  });
   it("should return Tuesday when being in #training-dienstag channel", function () {
     global.SpreadsheetApp = {
       getActiveSpreadsheet: () => ({
@@ -35,52 +72,13 @@ describe("`/dabei` command", function () {
 
     if (thisWeeksTuesday < Date.now()) {
       console.log("Expect next weeks Tuesday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksTuesday, "Dienstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksTuesday, "Dienstag"));
     } else {
       console.log("Expect this weeks Tuesday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksTuesday, "Dienstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksTuesday, "Dienstag"));
     }
-    assert.equal(returnValue[1], true);
+    assert.strictEqual(returnValue[1], true);
   });
-
-  it("should say you're already participating on Tuesday's", function () {
-    global.SpreadsheetApp = {
-      getActiveSpreadsheet: () => ({
-        getSheetByName: () => ({
-          appendRow: () => ({}),
-          getDataRange: () => ({
-            getValues: () => [
-              [""], ["max mustermann", 1337], ["alina musterfrau", 1337]
-            ]
-          }),
-        }),
-      }),
-    };
-
-
-    const requestData = {
-      parameter: {
-        user_name: "max mustermann",
-        command: "/dabei",
-        channel_id: "C012C7UEX9C",
-        channel_name: "training-dienstag",
-      },
-    };
-
-    let thisWeeksTuesday = getThisWeeksDay(false, 2);
-    let nextWeeksTuesday = getThisWeeksDay(true, 2);
-    let returnValue = participate(requestData);
-
-    if (thisWeeksTuesday < Date.now()) {
-      console.log("Expect next weeks Tuesday");
-      assert.equal(returnValue[0], createdExpectedOutputForAlreadyParticipating(nextWeeksTuesday, "Dienstag"));
-    } else {
-      console.log("Expect this weeks Tuesday");
-      assert.equal(returnValue[0], createdExpectedOutputForAlreadyParticipating(thisWeeksTuesday, "Dienstag"));
-    }
-    assert.equal(returnValue[1], false);
-  })
-
   it("should return Thursday when being in #training-donnerstag channel", function () {
     global.SpreadsheetApp = {
       getActiveSpreadsheet: () => ({
@@ -111,15 +109,15 @@ describe("`/dabei` command", function () {
 
     if (thisWeeksThursday < Date.now()) {
       console.log("Expect next weeks Thursday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksThursday, "Donnerstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksThursday, "Donnerstag"));
     } else {
       console.log("Expect this weeks Tuesday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksThursday, "Donnerstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksThursday, "Donnerstag"));
     }
-    assert.equal(returnValue[1], true);
+    assert.strictEqual(returnValue[1], true);
   });
 
-  it("should return Saturday when being in #training-saturday channel", function () {
+  it("should return Saturday when being in #training-samstag channel", function () {
     global.SpreadsheetApp = {
       getActiveSpreadsheet: () => ({
         getSheetByName: () => ({
@@ -148,13 +146,86 @@ describe("`/dabei` command", function () {
 
     if (thisWeeksSaturday < Date.now()) {
       console.log("Expect next weeks Saturday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksSaturday, "Samstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksSaturday, "Samstag"));
     } else {
       console.log("Expect this weeks Saturday");
-      assert.equal(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksSaturday, "Samstag"));
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksSaturday, "Samstag"));
     }
-    assert.equal(returnValue[1], true);
+    assert.strictEqual(returnValue[1], true);
   });
+  it("should return Sunday when being in #training-sonntag channel", function () {
+    global.SpreadsheetApp = {
+      getActiveSpreadsheet: () => ({
+        getSheetByName: () => ({
+          appendRow: () => ({}),
+          getDataRange: () => ({
+            getValues: () => [
+              [""], ["max mustermann", 1337], ["alina musterfrau", 1337]
+            ]
+          }),
+        }),
+      }),
+    };
+
+    const e = {
+      parameter: {
+        user_name: "wolfgang",
+        command: "/dabei",
+        channel_id: "C01D7L86K8Q",
+        channel_name: "training-samstag",
+      },
+    };
+
+    let thisWeeksSaturday = getThisWeeksDay(false, 7);
+    let nextWeeksSaturday = getThisWeeksDay(true, 7);
+    let returnValue = participate(e);
+
+    if (thisWeeksSaturday < Date.now()) {
+      console.log("Expect next weeks Saturday");
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(nextWeeksSaturday, "Sonntag"));
+    } else {
+      console.log("Expect this weeks Saturday");
+      assert.strictEqual(returnValue[0], createExpectedOutputForSuccessfulWrite(thisWeeksSaturday, "Sonntag"));
+    }
+    assert.strictEqual(returnValue[1], true);
+  });
+  it("should say you're already participating on Tuesday's", function () {
+    global.SpreadsheetApp = {
+      getActiveSpreadsheet: () => ({
+        getSheetByName: () => ({
+          appendRow: () => ({}),
+          getDataRange: () => ({
+            getValues: () => [
+              [""], ["max mustermann", 1337], ["alina musterfrau", 1337]
+            ]
+          }),
+        }),
+      }),
+    };
+
+
+    const requestData = {
+      parameter: {
+        user_name: "max mustermann",
+        command: "/dabei",
+        channel_id: "C012C7UEX9C",
+        channel_name: "training-dienstag",
+      },
+    };
+
+    let thisWeeksTuesday = getThisWeeksDay(false, 2);
+    let nextWeeksTuesday = getThisWeeksDay(true, 2);
+    let returnValue = participate(requestData);
+
+    if (thisWeeksTuesday < Date.now()) {
+      console.log("Expect next weeks Tuesday");
+      assert.strictEqual(returnValue[0], createdExpectedOutputForAlreadyParticipating(nextWeeksTuesday, "Dienstag"));
+    } else {
+      console.log("Expect this weeks Tuesday");
+      assert.strictEqual(returnValue[0], createdExpectedOutputForAlreadyParticipating(thisWeeksTuesday, "Dienstag"));
+    }
+    assert.strictEqual(returnValue[1], false);
+  })
 });
 
 function getThisWeeksDay(plusOneWeek, dayConstant) {
